11c57af2fe6dc9d2cbd45db1e37774ab265c07d4
Alex <harding1996@gmail.com>
2020-12-16 21:08:32 -0700

    removed unused imports from the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 /* Components */
@@ -11,8 +10,6 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-// import { SignInComponent } from './sign-in/sign-in.component';
-// import { SignUpComponent } from './sign-in/sign-up/sign-up.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
@@ -20,7 +17,6 @@ import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     NgbModule,
     HttpClientModule,
     AppRoutingModule,
@@ -31,8 +27,6 @@ import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
     FooterComponent,
     WelcomeComponent,
     NotFoundComponent,
-    // SignInComponent,
-    // SignUpComponent,
     ConfirmModalComponent,
   ],
   bootstrap: [AppComponent],
