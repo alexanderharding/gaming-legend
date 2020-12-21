c1917752d59a4ef71f9df84e53745c5f9239550f
Alex <harding1996@gmail.com>
2020-12-20 20:17:02 -0700

    added validationMessages to SignUpComponent

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -10,42 +10,96 @@ import { passwordMatcher } from 'src/app/functions/password-matcher';
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
+  submitted = true;
+
+  private readonly nameMinLength = 3;
+  private readonly nameMaxLength = 20;
+
+  private readonly firstNameValidationMessages = {
+    required: 'Please enter your first name.',
+    minlength: `First name must be longer than ${this.nameMinLength - 1}
+    characters.`,
+    maxlength: `First name cannot be longer than ${this.nameMaxLength}
+    characters.`,
+  };
+  firstNameMessage = this.firstNameValidationMessages['required'];
+
+  private readonly lastNameValidationMessages = {
+    required: 'Please enter your last name.',
+    minlength: `Last name must be longer than ${this.nameMinLength - 1}
+    characters.`,
+    maxlength: `Last name cannot be longer than ${this.nameMaxLength}
+    characters.`,
+  };
+  lastNameMessage = this.lastNameValidationMessages['required'];
+
+  private readonly phonePattern = /(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})/g;
+  private readonly phoneValidationMessages = {
+    required: 'Please enter your phone number.',
+    pattern: 'Please enter a valid phone number.',
+  };
+  phoneMessage = this.phoneValidationMessages['required'];
+
+  private readonly emailValidationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address. ie. fake@1234.com',
+  };
+  emailMessage = this.emailValidationMessages['required'];
+
+  private readonly confirmEmailValidationMessages = {
+    required: 'Please confirm your email address.',
+  };
+  confirmEmailMessage = this.confirmEmailValidationMessages['required'];
+
+  private readonly contactValidationMessages = {
+    match: 'The confirmation does not match your email address.',
+  };
+  contactMessage = this.contactValidationMessages['match'];
+
+  private readonly passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
 
   constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.signUpForm = this.fb.group(
-      {
-        firstName: [
-          '',
-          [
-            Validators.required,
-            // Validators.minLength(this.nameMinLength),
-            // Validators.maxLength(this.nameMaxLength),
+    this.signUpForm = this.fb.group({
+      contactGroup: this.fb.group(
+        {
+          firstName: [
+            '',
+            [
+              Validators.required,
+              Validators.minLength(this.nameMinLength),
+              Validators.maxLength(this.nameMaxLength),
+            ],
           ],
-        ],
-        lastName: [
-          '',
-          [
-            Validators.required,
-            // Validators.minLength(this.nameMinLength),
-            // Validators.maxLength(this.nameMaxLength),
+          lastName: [
+            '',
+            [
+              Validators.required,
+              Validators.minLength(this.nameMinLength),
+              Validators.maxLength(this.nameMaxLength),
+            ],
           ],
-        ],
-
-        email: ['', [Validators.required, Validators.email]],
-        confirmEmail: ['', Validators.required],
-        password: [
-          '',
-          [
-            Validators.required,
-            // Validators.pattern(this.passwordPattern)
+          phone: [
+            '',
+            [Validators.required, Validators.pattern(this.phonePattern)],
           ],
-        ],
-        confirmPassword: ['', [Validators.required]],
-        showPassword: false,
-      },
-      { validators: [emailMatcher, passwordMatcher] }
-    );
+          email: ['', [Validators.required, Validators.email]],
+          confirmEmail: ['', Validators.required],
+        },
+        { validator: emailMatcher }
+      ),
+      passwordGroup: this.fb.group(
+        {
+          password: [
+            '',
+            [Validators.required, Validators.pattern(this.passwordPattern)],
+          ],
+          confirmPassword: ['', [Validators.required]],
+          showPassword: false,
+        },
+        { validator: passwordMatcher }
+      ),
+    });
   }
 }
