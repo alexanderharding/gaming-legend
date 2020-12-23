96027eecaf167ad2c471a88c3b2424fa3e21862b
Alex <harding1996@gmail.com>
2020-12-22 21:54:42 -0700

    created CartSummaryComponent

---
+++ src/app/cart/cart-summary/cart-summary.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'ctacu-cart-summary',
+  templateUrl: './cart-summary.component.html',
+  styleUrls: ['./cart-summary.component.scss'],
+})
+export class CartSummaryComponent implements OnInit {
+  readonly tax = +this.cartService.tax;
+  readonly items$ = this.cartService.cartAction$;
+  readonly quantity$ = this.cartService.cartQuantity$;
+  readonly subtotal$ = this.cartService.subtotal$;
+  readonly totalTax$ = this.cartService.totalTax$;
+  readonly total$ = this.cartService.total$;
+  readonly shippingPrice$ = this.cartService.shippingSelectedAction$;
+
+  constructor(private readonly cartService: CartService) {}
+
+  ngOnInit(): void {}
+}
--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -16,6 +16,7 @@ import { CheckOutGuard } from './check-out/check-out.guard';
 import { ShippingRatesResolverService } from '../router/shipping-rates-resolver.service';
 import { ShippingPanelContentComponent } from './check-out/shipping-panel-content/shipping-panel-content.component';
 import { FinalizePanelContentComponent } from './check-out/finalize-panel-content/finalize-panel-content.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
 
 /* Routes */
 const ROUTES: Routes = [
@@ -52,6 +53,7 @@ const ROUTES: Routes = [
     ContactPanelContent,
     ShippingPanelContentComponent,
     FinalizePanelContentComponent,
+    CartSummaryComponent,
   ],
   providers: [CheckOutGuard, ShippingRatesResolverService],
 })
