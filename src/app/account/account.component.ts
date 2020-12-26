554a661c876917ed2d70bf78fdbdb791ff10d6a2
Alex <harding1996@gmail.com>
2020-12-26 12:47:07 -0700

    created editPasswordForm

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { passwordMatcher } from '../functions/password-matcher';
 import { AuthService } from '../services/auth.service';
+import { FormValidationRuleService } from '../services/form-validation-rule.service';
 
 @Component({
   selector: 'ctacu-account',
@@ -8,14 +11,35 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+  submitted = false;
   readonly user$ = this.authService.currentUser$;
+  private readonly passwordPattern = this.formValidationRuleService
+    .passwordPattern as RegExp;
+
+  editPasswordForm: FormGroup;
 
   constructor(
     private readonly authService: AuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly fb: FormBuilder,
+    private readonly formValidationRuleService: FormValidationRuleService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.editPasswordForm = this.fb.group({
+      currentPassword: ['', Validators.required],
+      passwordGroup: this.fb.group(
+        {
+          password: [
+            '',
+            [Validators.required, Validators.pattern(this.passwordPattern)],
+          ],
+          confirmPassword: ['', [Validators.required]],
+        },
+        { validator: passwordMatcher }
+      ),
+    });
+  }
 
   signOut(): void {
     this.authService.signOut();
