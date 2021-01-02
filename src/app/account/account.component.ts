70b9ec7c01d003d96c64516586dc5eb030df117d
Alex <harding1996@gmail.com>
2021-01-02 11:51:05 -0700

    created filterForm and sub'd to valueChanges

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { IOrder } from '../types/order';
 import { OrdersResult } from '../types/orders-result';
@@ -9,7 +11,7 @@ import { OrdersResult } from '../types/orders-result';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit, OnDestroy {
   /* Get data from resolver */
   private readonly resolvedData = this.route.snapshot.data
     .resolvedData as OrdersResult;
@@ -18,14 +20,34 @@ export class AccountComponent {
   submitted = false;
   loading = false;
   readonly user$ = this.authService.currentUser$;
+  filterForm: FormGroup;
+
+  private readonly subscriptions: Subscription[] = [];
 
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router,
     private readonly route: ActivatedRoute,
+    private readonly fb: FormBuilder,
     private readonly config: NgbAccordionConfig
   ) {
     config.closeOthers = true;
+  }
+
+  ngOnInit(): void {
+    this.filterForm = this.fb.group({
+      search: '',
+      sort: null,
+    });
+
+    const searchControl = this.filterForm.get('search');
+    this.subscriptions.push(searchControl.valueChanges.subscribe());
+    const sortControl = this.filterForm.get('sort');
+    this.subscriptions.push(sortControl.valueChanges.subscribe());
+
+    this.filterForm.patchValue({
+      sort: 0,
+    });
   }
 
   setLoading(value: boolean): void {
@@ -35,5 +57,9 @@ export class AccountComponent {
   signOut(): void {
     this.authService.signOut();
     this.router.navigate(['/user']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 }
