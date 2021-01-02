b927d1035d90d606e87046dd19588c697c9d416c
Alex <harding1996@gmail.com>
2021-01-01 23:08:43 -0700

    created OrdersResolverService

---
+++ src/app/router/orders-resolver.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { OrderService } from '../services/order.service';
+import { OrdersResult } from '../types/orders-result';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersResolverService implements Resolve<OrdersResult> {
+  constructor(private readonly orderService: OrderService) {}
+
+  resolve(): Observable<OrdersResult> {
+    return this.orderService.orders$.pipe(
+      map((orders) => ({ orders } as OrdersResult)),
+      catchError((error) =>
+        of({
+          orders: null,
+          error: `Retrieval error: ${error}.`,
+        } as OrdersResult)
+      )
+    );
+  }
+}
