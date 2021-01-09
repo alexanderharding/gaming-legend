1b9854d8009fa9c10fc0db2f6a67ee56268c06b3
Alex <harding1996@gmail.com>
2021-01-08 21:59:30 -0700

    created notificationSubject and notifications$

--- src/app/notifications-container/notifications-container.component.ts
+++ src/app/notifications-container/notifications-container.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  TemplateRef,
+} from '@angular/core';
 import { NotificationService } from '../services/notification.service';
-import { INotification } from '../types/notification';
 
 @Component({
   selector: 'ctacu-notifications-container',
   templateUrl: './notifications-container.component.html',
   styleUrls: ['./notifications-container.component.scss'],
   host: { '[class.ngb-toasts]': 'true' },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotificationsContainerComponent implements OnInit {
-  get notifications(): INotification[] {
-    return this.notificationService.notifications;
-  }
+export class NotificationsContainerComponent {
+  notifications$ = this.notificationService.notifications$;
 
   constructor(private readonly notificationService: NotificationService) {}
-
-  ngOnInit(): void {}
-
-  isTemplate(notification) {
-    return notification.textOrTpl instanceof TemplateRef;
-  }
 
   remove(notification): void {
     this.notificationService.remove(notification);
--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,19 +1,26 @@
 import { Injectable, TemplateRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { INotification } from '../types/notification';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  notifications: INotification[] = [];
+  private notifications: INotification[] = [];
+  private readonly notificationSubject = new BehaviorSubject<INotification[]>(
+    this.notifications
+  );
+  readonly notifications$ = this.notificationSubject.asObservable();
 
   constructor() {}
 
   show(notification: INotification): void {
     this.notifications.push(notification);
+    this.notificationSubject.next(this.notifications);
   }
 
   remove(notification: INotification): void {
     this.notifications = this.notifications.filter((n) => n !== notification);
+    this.notificationSubject.next(this.notifications);
   }
 }
