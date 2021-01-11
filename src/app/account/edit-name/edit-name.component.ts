7ea5f9c6aee3edf22f73348ed8389553b97e8a01
Alex <harding1996@gmail.com>
2021-01-10 17:20:53 -0700

    added ngbCollapse to EditNameComponent

--- src/app/account/edit-name/edit-name.component.ts
+++ src/app/account/edit-name/edit-name.component.ts
@@ -2,14 +2,19 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnDestroy,
   OnInit,
   Output,
   TemplateRef,
   ViewChild,
 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { first, tap } from 'rxjs/operators';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { passwordChecker } from 'src/app/functions/password-checker';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormValidationRuleService } from 'src/app/services/form-validation-rule.service';
@@ -22,9 +27,12 @@ import { IUser, User } from 'src/app/types/user';
   templateUrl: './edit-name.component.html',
   styleUrls: ['./edit-name.component.scss'],
 })
-export class EditNameComponent implements OnInit {
+export class EditNameComponent implements OnInit, OnDestroy {
   submitted = false;
   editForm: FormGroup;
+  hasValueChanged = false;
+
+  private subscription: Subscription;
 
   @ViewChild('successTpl') private successTpl: TemplateRef<any>;
   @ViewChild('dangerTpl') private dangerTpl: TemplateRef<any>;
@@ -36,14 +44,6 @@ export class EditNameComponent implements OnInit {
 
   readonly nameMinLength = +this.formValidationRuleService.nameMinLength;
   readonly nameMaxLength = +this.formValidationRuleService.nameMaxLength;
-
-  get hasChanged(): boolean {
-    const formValue = JSON.stringify(
-      this.editForm.get('nameGroup').value
-    ).toLowerCase();
-    const userNameValue = JSON.stringify(this.user.name).toLowerCase();
-    return formValue !== userNameValue;
-  }
 
   constructor(
     private readonly fb: FormBuilder,
@@ -77,6 +77,10 @@ export class EditNameComponent implements OnInit {
         ],
       }),
     });
+    const nameControl = this.editForm.get('nameGroup');
+    this.subscription = nameControl.valueChanges.subscribe(() =>
+      this.setHasValueChanged(nameControl)
+    );
   }
 
   onSubmit(form: FormGroup): void {
@@ -98,6 +102,12 @@ export class EditNameComponent implements OnInit {
       firstName: name.firstName,
       lastName: name.lastName,
     });
+  }
+
+  private setHasValueChanged(c: AbstractControl): void {
+    const controlValue = JSON.stringify(c.value).toLowerCase();
+    const userNameValue = JSON.stringify(this.user.name).toLowerCase();
+    this.hasValueChanged = controlValue === userNameValue ? false : true;
   }
 
   private showSuccess(): void {
@@ -137,5 +147,9 @@ export class EditNameComponent implements OnInit {
         this.showDanger();
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
