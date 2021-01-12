45b536498f8f7d15cc5b8c2a56316425076f34f5
Alex <harding1996@gmail.com>
2021-01-12 14:49:00 -0700

    started to add testing to OrderService

--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { Order } from '../types/order';
 
 import { OrderService } from './order.service';
 
-describe('OrderService', () => {
-  let service: OrderService;
+fdescribe('OrderService', () => {
+  let order: Order;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrderService);
+    TestBed.configureTestingModule({
+      providers: [OrderService],
+      imports: [HttpClientTestingModule],
+    });
   });
 
-  it('should be created', () => {
+  it('should be created', inject([OrderService], (service: OrderService) => {
     expect(service).toBeTruthy();
+  }));
+
+  describe('getOrder', () => {
+    it('should call http client with the correct url', inject(
+      [OrderService, HttpTestingController],
+      (service: OrderService, controller: HttpTestingController) => {
+        // Arrange
+
+        // Act
+        service.getOrder(1).subscribe();
+
+        // Assert
+        controller.expectOne(`http://localhost:3000/orders/${1}`);
+        controller.verify();
+      }
+    ));
   });
+
+  // describe('saveItem', () => {
+  //   it(`should call http client with the correct url when order has no id`, inject(
+  //     [OrderService, HttpTestingController],
+  //     (service: OrderService, controller: HttpTestingController) => {
+  //       // Arrange
+
+  //       // Act
+  //       service.saveOrder(order, order.id).subscribe();
+
+  //       // Assert
+  //       controller.expectOne(`http://localhost:3000/orders`);
+  //       controller.verify();
+  //     }
+  //   ));
+  // });
 });
