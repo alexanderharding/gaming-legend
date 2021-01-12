ef9b0094769d92ec2a8b826e24cb42159b3699e9
Alex <harding1996@gmail.com>
2021-01-12 16:00:36 -0700

    added testing to NotificationService

--- src/app/services/notification.service.spec.ts
+++ src/app/services/notification.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { INotification } from '../types/notification';
 
 import { NotificationService } from './notification.service';
 
 describe('NotificationService', () => {
   let service: NotificationService;
+  let notifications: INotification[];
+  let notification: INotification;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +15,53 @@ describe('NotificationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('notifications$', () => {
+    it('should have no notifications to start', () => {
+      // Arrange
+      notifications = [];
+
+      // Act
+      service.notifications$.subscribe((n) => (notifications = n));
+
+      //Assert
+      expect(notifications.length).toBe(0);
+    });
+
+    it('should add a notification when show is called', () => {
+      // Arrange
+      notifications = [];
+      notification = {
+        textOrTpl: 'Success message!',
+        className: 'bg-success text-light',
+      };
+
+      // Act
+      service.show(notification);
+      service.notifications$.subscribe((n) => (notifications = n));
+
+      // Assert
+      expect(notifications.length).toBe(1);
+    });
+
+    it('should remove notification when remove is called', () => {
+      // Arrange
+      notifications = [];
+      notification = {
+        textOrTpl: 'Success message!',
+        className: 'bg-success text-light',
+      };
+      service.show(notification);
+      service.notifications$.subscribe((n) => (notifications = n));
+      expect(notifications.length).toBe(1);
+
+      // Act
+      service.remove(notification);
+      service.notifications$.subscribe((n) => (notifications = n));
+
+      // Assert
+      expect(notifications.length).toBe(0);
+    });
   });
 });
