297e7c6079cb2859488b4240803a99f6d9aa18ac
Alex <harding1996@gmail.com>
2021-01-16 15:26:16 -0700

    created tests for SuccesComponent

--- src/app/cart/success/success.component.spec.ts
+++ src/app/cart/success/success.component.spec.ts
@@ -1,4 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { SuccessComponent } from './success.component';
 
@@ -6,20 +8,74 @@ describe('SuccessComponent', () => {
   let component: SuccessComponent;
   let fixture: ComponentFixture<SuccessComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SuccessComponent ]
+  @Component({
+    selector: 'ngb-alert',
+    template: '<div></div>',
+  })
+  class FakeNgbAlertComponent {}
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SuccessComponent, FakeNgbAlertComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SuccessComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('should set the pageTitle corrently', () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(component.pageTitle).toBe('Order Placed');
+  });
+});
+
+describe('SuccessComponent w/ template', () => {
+  let component: SuccessComponent;
+  let fixture: ComponentFixture<SuccessComponent>;
+
+  @Component({
+    selector: 'ngb-alert',
+    template: '<div></div>',
+  })
+  class FakeNgbAlertComponent {}
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SuccessComponent, FakeNgbAlertComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuccessComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should set the pageTitle in the template', () => {
+    // Arrange
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const elements = fixture.debugElement.queryAll(By.css('h1'));
+    expect(elements[0].nativeElement.textContent).toContain(
+      component.pageTitle
+    );
   });
 });
