a94066f60968ad51deaaa1e89a556f8d07ab0b3f
Alex <harding1996@gmail.com>
2021-01-16 19:26:00 -0700

    created payment class test

--- src/app/types/payment.spec.ts
+++ src/app/types/payment.spec.ts
@@ -1,7 +1,26 @@
 import { Payment } from './payment';
 
 describe('Payment', () => {
-  // it('should create an instance', () => {
-  //   expect(new Payment()).toBeTruthy();
-  // });
+  const cardNumber = 1214121474125;
+  const cvv = 210;
+  const expiringMonth = 1;
+  const expiringYear = 2022;
+  const subtotal = 123.33;
+  const tax = 100.25;
+  const shipping = 6.99;
+  const total = 205.28;
+  it('should create an instance', () => {
+    expect(
+      new Payment(
+        cardNumber,
+        cvv,
+        expiringMonth,
+        expiringYear,
+        subtotal,
+        tax,
+        shipping,
+        total
+      )
+    ).toBeTruthy();
+  });
 });
---
+++ src/app/types/product-brands-result.spec.ts
@@ -0,0 +1,7 @@
+import { ProductBrandsResult } from './product-brands-result';
+
+fdescribe('ProductBrandsResult', () => {
+  it('should create an instance', () => {
+    expect(new ProductBrandsResult()).toBeTruthy();
+  });
+});
