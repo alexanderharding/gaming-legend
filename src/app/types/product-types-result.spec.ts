43f56f8e0d270ff540b11523f652dd24c8208175
Alex <harding1996@gmail.com>
2021-01-16 19:31:04 -0700

    unfocused ProductTypesResult test

--- src/app/types/product-types-result.spec.ts
+++ src/app/types/product-types-result.spec.ts
@@ -1,6 +1,6 @@
 import { ProductTypesResult } from './product-types-result';
 
-fdescribe('ProductTypesResult', () => {
+describe('ProductTypesResult', () => {
   it('should create an instance', () => {
     expect(new ProductTypesResult()).toBeTruthy();
   });
