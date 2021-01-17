1351513040742564060f19f2ed4d60656d51cb6d
Alex <harding1996@gmail.com>
2021-01-16 20:04:24 -0700

    added UserNameMaker test

--- src/app/types/user-name.spec.ts
+++ src/app/types/user-name.spec.ts
@@ -1,9 +1,15 @@
-import { UserName } from './user-name';
+import { UserName, UserNameMaker } from './user-name';
 
 describe('UserName', () => {
   const firstName = 'John';
   const lastName = 'Doe';
   it('should create an instance', () => {
     expect(new UserName(firstName, lastName)).toBeTruthy();
+  });
+});
+
+describe('UserNameMaker', () => {
+  it('should create an instance', () => {
+    expect(new UserNameMaker()).toBeTruthy();
   });
 });
