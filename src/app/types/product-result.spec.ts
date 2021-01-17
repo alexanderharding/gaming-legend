98d97540c1a5a269535912d36ccb817df2260656
Alex <harding1996@gmail.com>
2021-01-16 19:43:58 -0700

    created product-result.spec.ts

---
+++ src/app/types/product-result.spec.ts
@@ -0,0 +1,7 @@
+import { ProductResult } from './product-result';
+
+fdescribe('ProductResult', () => {
+  it('should create an instance', () => {
+    expect(new ProductResult()).toBeTruthy();
+  });
+});
