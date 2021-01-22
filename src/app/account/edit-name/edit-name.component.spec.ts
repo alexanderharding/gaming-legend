3ba82d2644273430e9e39744b78978946a6fbd31
Alex <harding1996@gmail.com>
2021-01-21 23:06:25 -0700

    started to set up testing for EditNameComponent

--- src/app/account/edit-name/edit-name.component.spec.ts
+++ src/app/account/edit-name/edit-name.component.spec.ts
@@ -1,20 +1,83 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IUser } from 'src/app/types/user';
 
 import { EditNameComponent } from './edit-name.component';
 
-xdescribe('EditNameComponent', () => {
+describe('EditNameComponent', () => {
   let component: EditNameComponent;
   let fixture: ComponentFixture<EditNameComponent>;
+  let mockAuthService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditNameComponent],
-    }).compileComponents();
-  }));
+  const USER: IUser = {
+    name: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    contact: {
+      phone: '8011231234',
+      email: 'test@test.com',
+    },
+    address: {
+      street: '123 S Bend Ct',
+      city: 'Las Vegas',
+      state: 'Nevada',
+      zip: '12345',
+      country: 'USA',
+    },
+    password: 'TestPassword1234',
+    isAdmin: true,
+    id: 121014,
+  };
+
+  @Component({
+    selector: 'ctacu-current-password-form',
+    template: '<div></div>',
+  })
+  class FakeCurrentPasswordFormComponent {
+    @Input() parentForm: FormGroup;
+    @Input() submitted: FormGroup;
+  }
+
+  @Component({
+    selector: 'ctacu-name-form',
+    template: '<div></div>',
+  })
+  class FakeNameFormComponent {
+    @Input() parentForm: FormGroup;
+    @Input() submitted: FormGroup;
+    @Input() pageTitle: string;
+    @Input() user: IUser;
+  }
+
+  beforeEach(
+    waitForAsync(() => {
+      mockAuthService = jasmine.createSpyObj(['']);
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientTestingModule,
+          ReactiveFormsModule,
+          FormsModule,
+          NgbModule,
+        ],
+        declarations: [
+          EditNameComponent,
+          FakeNameFormComponent,
+          FakeCurrentPasswordFormComponent,
+        ],
+        // schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditNameComponent);
     component = fixture.componentInstance;
+    component.user = USER;
+    component.loading = false;
     fixture.detectChanges();
   });
 
