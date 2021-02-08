53529ca9e95bcb894a10fc62f046bf27d8a2e5b1
Alex <harding1996@gmail.com>
2021-02-08 15:56:41 -0700

    added testing to WelcomeComponent

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { WelcomeComponent } from './welcome.component';
 
@@ -22,5 +23,42 @@ describe('WelcomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set pageTitle correctly', () => {
+    expect(component.pageTitle).toBe('Gaming Legend');
+  });
+});
+
+describe('WelcomeComponent w/ template', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [WelcomeComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set pageTitle correctly in the template', () => {
+    const elements = fixture.debugElement.queryAll(By.css('p'));
+
+    expect(elements.length).toBe(2);
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      expect(element.nativeElement.textContent).toContain(component.pageTitle);
+    }
   });
 });
