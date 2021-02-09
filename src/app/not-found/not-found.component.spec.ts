17c990d44d2eb6d15b1953bcfdeac99afde3b5ac
Alex <harding1996@gmail.com>
2021-02-08 17:42:24 -0700

    added testing for NotFoundComponent

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,24 +1,17 @@
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
 
-  // @Component({
-  //   selector: 'ngb-alert',
-  //   template: '<div></div>',
-  // })
-  // class FakeNgbAlertComponent {}
-
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        declarations: [
-          NotFoundComponent,
-          // FakeNgbAlertComponent
-        ],
+        imports: [NgbModule],
+        declarations: [NotFoundComponent],
       }).compileComponents();
     })
   );
@@ -31,5 +24,39 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have set pageTitle correctly', () => {
+    expect(component.pageTitle).toBe('Page Not Found');
+  });
+});
+
+describe('NotFoundComponent w/ template', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [NgbModule],
+        declarations: [NotFoundComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set pageTitle correctly in the template', () => {
+    const elements = fixture.debugElement.queryAll(By.css('h1'));
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toBe(component.pageTitle);
   });
 });
