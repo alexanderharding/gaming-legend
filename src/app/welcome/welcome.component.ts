b5a6a19dd0ba1849d34e4e5301a6afafa02495b0
Alex <harding1996@gmail.com>
2021-02-11 19:39:54 -0700

    added Title service to WelcomeComponent

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
   readonly pageTitle = 'Gaming Legend';
 
-  constructor() {}
+  constructor(private readonly title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle(`${this.pageTitle} | Welcome`);
+  }
 }
