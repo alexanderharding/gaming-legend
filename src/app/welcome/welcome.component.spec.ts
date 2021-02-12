fdbaed3d1ec859cfb769e4d1a6528159cd2705ee
Alex <harding1996@gmail.com>
2021-02-11 19:40:35 -0700

    updated WelcomeComponent's tests

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,16 +1,20 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { By, Title } from '@angular/platform-browser';
 
 import { WelcomeComponent } from './welcome.component';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  let mockTitle: Title;
 
   beforeEach(
     waitForAsync(() => {
+      mockTitle = jasmine.createSpyObj(['setTitle']);
       TestBed.configureTestingModule({
         declarations: [WelcomeComponent],
+        providers: [{ provide: Title, useValue: mockTitle }],
       }).compileComponents();
     })
   );
@@ -18,15 +22,28 @@ describe('WelcomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
 
   it('should set pageTitle correctly', () => {
+    fixture.detectChanges();
+
     expect(component.pageTitle).toBe('Gaming Legend');
+  });
+
+  it(`should have called setTitle method on Title service with correct
+    value`, () => {
+    fixture.detectChanges();
+
+    expect(mockTitle.setTitle).toHaveBeenCalledTimes(1);
+    expect(mockTitle.setTitle).toHaveBeenCalledWith(
+      `${component.pageTitle} | Welcome`
+    );
   });
 });
 
@@ -45,19 +62,26 @@ describe('WelcomeComponent w/ template', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
 
   it('should set pageTitle correctly in the template', () => {
-    const elements = fixture.debugElement.queryAll(By.css('p'));
+    let elements: DebugElement[];
 
-    expect(elements.length).toBe(2);
-    for (const element of elements) {
-      expect(element.nativeElement.textContent).toContain(component.pageTitle);
-    }
+    fixture.detectChanges();
+
+    elements = fixture.debugElement.queryAll(By.css('h1'));
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toBe(component.pageTitle);
+    elements = fixture.debugElement.queryAll(By.css('p'));
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toContain(
+      component.pageTitle
+    );
   });
 });
