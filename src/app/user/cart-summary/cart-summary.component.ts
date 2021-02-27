b211fde48a98a2c688607f89072dfd8f16c7785a
Alex <harding1996@gmail.com>
2021-02-26 21:35:05 -0700

    Restyled CartSummaryComponent

--- src/app/user/cart-summary/cart-summary.component.ts
+++ src/app/user/cart-summary/cart-summary.component.ts
@@ -1,25 +1,67 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 import { CartService } from 'src/app/services/cart.service';
 import { ShippingRateService } from 'src/app/services/shipping-rate.service';
+import { IShipping } from 'src/app/types/shipping';
 
 @Component({
   selector: 'ctacu-cart-summary',
   templateUrl: './cart-summary.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CartSummaryComponent {
-  readonly tax = +this.cartService.tax;
+export class CartSummaryComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
   readonly items$ = this.cartService.cartItems$;
-  readonly quantity$ = this.cartService.cartQuantity$;
   readonly subtotal$ = this.cartService.subtotal$;
   readonly totalTax$ = this.cartService.totalTax$;
   readonly total$ = this.cartService.total$;
   readonly shippingPrice$ = this.shippingRateService
     .shippingPriceSelectedAction$;
+  shippingForm: FormGroup;
+
+  @Input() shippingRates: IShipping[];
 
   constructor(
     private readonly cartService: CartService,
-    private readonly shippingRateService: ShippingRateService
+    private readonly shippingRateService: ShippingRateService,
+    private readonly fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.shippingForm = this.fb.group({
+      price: null,
+    });
+
+    const priceControl = this.shippingForm.controls.price;
+    this.subscriptions.push(
+      priceControl.valueChanges.subscribe((price) =>
+        this.shippingRateService.setShipping(+price)
+      )
+    );
+
+    this.shippingPrice$
+      .pipe(first())
+      .subscribe((p) =>
+        p
+          ? priceControl.setValue(+p)
+          : priceControl.setValue(+this.shippingRates[0].price)
+      );
+  }
+
+  getDeliveryDate(days: number): Date {
+    return this.shippingRateService.getDeliveryDate(+days);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe);
+  }
 }
