14ccaa9ee8be1094e80f810391e5f89303ab7e1b
Alex <harding1996@gmail.com>
2021-02-28 22:49:02 -0700

    added more responsive styling

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,11 +7,17 @@ import { Title } from '@angular/platform-browser';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomeComponent implements OnInit {
-  readonly pageTitle = 'Gaming Legend';
+  readonly pageTitle = 'Welcome';
+  readonly welcomeMessage = `A top-of-the-line PC will outpace many gaming
+    consoles when it comes to sheer computing power, but many low- to mid-tier
+    computers also provide great gaming experiences. PC gaming is largely about
+    customization, giving you a vast array of hardware and accessories to choose
+    from. No matter how you want to game, Gaming Legend has the perfect
+    hardware for you.`;
 
   constructor(private readonly title: Title) {}
 
   ngOnInit(): void {
-    this.title.setTitle(`${this.pageTitle} | Welcome`);
+    this.title.setTitle(`Gaming Legend | ${this.pageTitle}`);
   }
 }
