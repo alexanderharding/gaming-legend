00fecfe11727f7ca52ae9cdf64844f82175c0cbe
Alex <harding1996@gmail.com>
2021-03-02 20:02:44 -0700

    updated tests to match new styling

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -30,19 +30,30 @@ describe('WelcomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set pageTitle correctly', () => {
+  it('should have set pageTitle correctly', () => {
     fixture.detectChanges();
 
-    expect(component.pageTitle).toBe('Gaming Legend');
+    expect(component.pageTitle).toBe('Welcome');
+  });
+
+  it('should have set welcomeMessage correctly', () => {
+    fixture.detectChanges();
+
+    expect(component.welcomeMessage)
+      .toBe(`A top-of-the-line PC will outpace many gaming
+    consoles when it comes to sheer computing power, but many low- to mid-tier
+    computers also provide great gaming experiences. PC gaming is largely about
+    customization, giving you a vast array of hardware and accessories to choose
+    from. No matter how you want to game, Gaming Legend has the perfect
+    hardware for you.`);
   });
 
   it(`should have called setTitle method on Title service with correct
     value`, () => {
     fixture.detectChanges();
 
-    expect(mockTitle.setTitle).toHaveBeenCalledTimes(1);
-    expect(mockTitle.setTitle).toHaveBeenCalledWith(
-      `${component.pageTitle} | Welcome`
+    expect(mockTitle.setTitle).toHaveBeenCalledOnceWith(
+      `Gaming Legend | ${component.pageTitle}`
     );
   });
 });
@@ -70,18 +81,40 @@ describe('WelcomeComponent w/ template', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set pageTitle correctly in the template', () => {
+  it('should set pageTitle in the template', () => {
     let elements: DebugElement[];
-
     fixture.detectChanges();
 
     elements = fixture.debugElement.queryAll(By.css('h1'));
     expect(elements.length).toBe(1);
+    expect(elements[0].classes).toEqual({
+      'display-4': true,
+      'd-none': true,
+      'd-sm-block': true,
+    });
     expect(elements[0].nativeElement.textContent).toBe(component.pageTitle);
-    elements = fixture.debugElement.queryAll(By.css('p'));
+    elements = fixture.debugElement.queryAll(By.css('h2'));
     expect(elements.length).toBe(1);
-    expect(elements[0].nativeElement.textContent).toContain(
-      component.pageTitle
+    expect(elements[0].classes).toEqual({
+      'd-sm-none': true,
+    });
+    expect(elements[0].nativeElement.textContent).toBe(component.pageTitle);
+  });
+
+  it('should set welcomeMessage in the template', () => {
+    let elements: DebugElement[];
+    fixture.detectChanges();
+
+    elements = fixture.debugElement.queryAll(By.css('p'));
+    expect(elements.length).toBe(2);
+    elements.forEach((e) =>
+      expect(e.nativeElement.textContent)
+        .toBe(`A top-of-the-line PC will outpace many gaming
+    consoles when it comes to sheer computing power, but many low- to mid-tier
+    computers also provide great gaming experiences. PC gaming is largely about
+    customization, giving you a vast array of hardware and accessories to choose
+    from. No matter how you want to game, Gaming Legend has the perfect
+    hardware for you.`)
     );
   });
 });
