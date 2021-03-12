8ced3b6810cac49fe6f1657ac72b37cdf755c2b0
Alex <harding1996@gmail.com>
2021-03-11 21:41:14 -0700

    updated ConfirmModalComponent testing

--- src/app/shared/confirm-modal/confirm-modal.component.spec.ts
+++ src/app/shared/confirm-modal/confirm-modal.component.spec.ts
@@ -29,24 +29,8 @@ describe('ConfirmModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have "confirm" as title to start', () => {
-    expect(component.title).toEqual('confirm');
-  });
-
   it('should have no message to start', () => {
     expect(component.message).toBeFalsy();
-  });
-
-  it('should have no warningMessage to start', () => {
-    expect(component.warningMessage).toBeFalsy();
-  });
-
-  it('should have no infoMessage to start', () => {
-    expect(component.infoMessage).toBeFalsy();
-  });
-
-  it('should have "bg-secondary" as type to start', () => {
-    expect(component.type).toEqual('bg-secondary');
   });
 
   it('should have "ok" as closeMessage message to start', () => {
@@ -98,18 +82,6 @@ describe('ConfirmModalComponent w/ template', () => {
     component = fixture.componentInstance;
   });
 
-  it('should set the title in the template', () => {
-    // Arrange
-    component.title = 'title';
-
-    // Act
-    fixture.detectChanges();
-
-    // Assert
-    const elements = fixture.debugElement.queryAll(By.css('h4'));
-    expect(elements[0].nativeElement.textContent).toContain(component.title);
-  });
-
   it('should set the message in the template', () => {
     // Arrange
     component.message = 'message';
@@ -120,50 +92,6 @@ describe('ConfirmModalComponent w/ template', () => {
     // Assert
     const elements = fixture.debugElement.queryAll(By.css('p strong'));
     expect(elements[0].nativeElement.textContent).toContain(component.message);
-  });
-
-  it('should set the warningMessage in the template', () => {
-    // Arrange
-    component.warningMessage = 'warning message';
-
-    // Act
-    fixture.detectChanges();
-
-    // Assert
-    const elements = fixture.debugElement.queryAll(By.css('p strong'));
-    expect(elements[1].nativeElement.textContent).toContain(
-      component.warningMessage
-    );
-  });
-
-  it('should set the infoMessage in the template w/o warning message', () => {
-    // Arrange
-    component.infoMessage = 'info message';
-    component.warningMessage = null;
-
-    // Act
-    fixture.detectChanges();
-
-    // Assert
-    const elements = fixture.debugElement.queryAll(By.css('p strong'));
-    expect(elements[1].nativeElement.textContent).toContain(
-      component.infoMessage
-    );
-  });
-
-  it('should set the infoMessage in the template w/ warning message', () => {
-    // Arrange
-    component.infoMessage = 'info message';
-    component.warningMessage = 'warning message';
-
-    // Act
-    fixture.detectChanges();
-
-    // Assert
-    const elements = fixture.debugElement.queryAll(By.css('p strong'));
-    expect(elements[2].nativeElement.textContent).toContain(
-      component.infoMessage
-    );
   });
 
   it('should set the closeMessage in the template', () => {
