6769a61c0eadbfd790015840beb8e2b92305dbd2
Alex <harding1996@gmail.com>
2021-03-20 22:15:04 -0600

    started CheckOutComponent testing

--- src/app/user/checkout/checkout.component.spec.ts
+++ src/app/user/checkout/checkout.component.spec.ts
@@ -1,24 +1,105 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, Input, Pipe, PipeTransform } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IShipping } from 'src/app/types/shipping';
+import { ShippingRatesResult } from 'src/app/types/shipping-rates-result';
 
 import { CheckoutComponent } from './checkout.component';
 
-xdescribe('CheckoutComponent', () => {
-  let component: CheckoutComponent;
-  let fixture: ComponentFixture<CheckoutComponent>;
+@Pipe({
+  name: 'capitalize',
+})
+class MockCapitalizePipe implements PipeTransform {
+  transform(value: string): string {
+    return value;
+  }
+}
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [CheckoutComponent],
-    }).compileComponents();
-  });
+describe('CheckoutComponent', () => {
+  @Component({
+    selector: 'ctacu-cart-summary',
+    template: '<div></div>',
+  })
+  class FakeCartSummaryComponent {
+    @Input() shippingRates: IShipping[];
+  }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CheckoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  @Component({
+    selector: 'ctacu-error-received',
+    template: '<div></div>',
+  })
+  class FakeErrorReceivedComponent {
+    @Input() errorMessage: string;
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('onShippingRatesReceived', () => {
+    let component: CheckoutComponent;
+    let fixture: ComponentFixture<CheckoutComponent>;
+    let mockActivatedRoute;
+
+    const SHIPPINGRATES: IShipping[] = [
+      {
+        id: 1,
+        days: 7,
+        price: 6.99,
+        title: 'Standard',
+      },
+      {
+        id: 2,
+        days: 3,
+        price: 14.99,
+        title: 'Fast',
+      },
+      {
+        id: 3,
+        days: 1,
+        price: 19.99,
+        title: 'Insanely Fast',
+      },
+    ];
+    const RESOLVEDDATA: ShippingRatesResult = {
+      shippingRates: SHIPPINGRATES,
+    };
+
+    beforeEach(
+      waitForAsync(() => {
+        mockActivatedRoute = jasmine.createSpyObj([], {
+          snapshot: {
+            data: {
+              resolvedData: RESOLVEDDATA,
+            },
+          },
+        });
+        TestBed.configureTestingModule({
+          imports: [
+            HttpClientTestingModule,
+            RouterTestingModule,
+            ReactiveFormsModule,
+          ],
+          declarations: [
+            CheckoutComponent,
+            MockCapitalizePipe,
+            FakeCartSummaryComponent,
+          ],
+          providers: [
+            { provide: ActivatedRoute, useValue: mockActivatedRoute },
+          ],
+        }).compileComponents();
+      })
+    );
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(CheckoutComponent);
+      component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+      fixture.detectChanges();
+
+      expect(component).toBeTruthy();
+    });
   });
 });
