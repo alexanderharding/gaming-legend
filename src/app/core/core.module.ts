a73af60b33a506dff7abead45856ba6ea7bcfe7d
Alex <harding1996@gmail.com>
2021-03-22 19:47:05 -0600

    moved NavbarComponent into the CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartService } from '../services/cart.service';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent],
   imports: [CommonModule, RouterModule, NgbModule],
+  exports: [NavbarComponent, WelcomeComponent],
+  declarations: [NavbarComponent, WelcomeComponent],
+  providers: [CartService],
 })
 export class CoreModule {}
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { CartService } from '../services/cart.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'ctacu-navbar',
