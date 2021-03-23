c97622059c70dafd21b281794646f096e7951109
Alex <harding1996@gmail.com>
2021-03-22 23:01:38 -0600

    removed subtotal tax and total from Payment class

--- src/app/types/payment.ts
+++ src/app/types/payment.ts
@@ -3,9 +3,6 @@ export class Payment {
     public cardNumber: number,
     public expiration: string,
     public cvv: number,
-    public subtotal: number,
-    public tax: number,
-    public shipping: number,
     public total: number
   ) {}
 }
@@ -16,9 +13,6 @@ export class PaymentMaker {
       cardNumber: event.cardNumber,
       expiration: event.expiration,
       cvv: event.cvv,
-      subtotal: event.subtotal,
-      tax: event.tax,
-      shipping: event.shipping,
       total: event.total,
     };
   }
